3cc1bbd5346abf76fa944a8a7b6f643a7a12b4f0
khazifire <itskazimz@gmail.com>
2022-08-12 15:04:52 +0300

    fixed: build errors

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -7,7 +7,7 @@ const Layout = ({children}) => {
         <>
         <Head>
             <meta charset="UTF-8"/>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <link rel="icon" href="/favicon.png" />
         </Head>
