5d9fe1575ca671ad4f93bed39acb0ac81181b2fd
khazifire <itskazimz@gmail.com>
2022-08-28 22:09:40 +0300

    [FIX]: updated hero heading

--- components/other/HeroSection.js
+++ components/other/HeroSection.js
@@ -10,8 +10,9 @@ const HeroSection = () => {
           <div className='relative flex flex-col gap-2 xl:w-1/2 text-drip-black'>
               <h1 className='text-5xl font-bold md:text-7xl font-Cursive'>DripUI</h1>
               <h2 className='text-3xl font-semibold md:text-4xl font-Cursive'>Tailwind CSS Components</h2>
-              <h3 className='text-xl md:text-2xl md:w-[60%] font-Inter'>DripUI is a collection of free Tailwind CSS components to bootstrap your new apps, projects or landing sites!</h3>
 
+              {/* <h3 className='text-xl md:text-2xl md:w-[60%] font-Inter'>DripUI is a collection of free Tailwind CSS components to bootstrap your new apps, projects or landing sites!</h3> */}
+              <h3 className='text-xl md:text-2xl md:w-[60%] font-Inter'>DripUI is a collection of free UI components to help you develop your component easier and better!</h3>
                <div className='absolute -top-12 right-24 md:left-56 -md:top-0 rotate-12 animate-pulse'>
                 <span className='text-xl italic font-semibold md:text-2xl font-Cursive '>Free Open Source </span>
                 <Arrow/>
