b6eece29f39f1ddb22eaddf683a132c3a2ada369
Dan <73513866+khazifire@users.noreply.github.com>
2022-09-30 15:34:23 +0700

    [update]: create the instalation process page

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -17,8 +17,12 @@ const Header = () => {
                     
                   </div>
 
-                  <div className="">
-                      <a target="_blank" rel="noreferrer" className="inline-flex items-center gap-2 text-sm md:text-lg group hover:text-drip-black/80" href="https://twitter.com/intent/tweet?url=https://khazifire.com&text=DripUI%20is%20a%20collection%20of%20free%20Tailwind%20CSS%20components%20that%20can%20be%20used%20in%20your%20next%20project.%20%0A-%20By%20@khazifire%0A"><i className="text-lg ri-twitter-line group-hover:-rotate-12 "></i> Share on twitter</a>
+                  <div className="flex flex-row gap-4">
+                    <Link href="/docs/how-to-use">
+                         <a className="pl-3 text-sm border-l-2 md:text-lg border-drip-black/20 hover:text-drip-black/80">How to use</a>
+                    </Link>
+
+                    <a target="_blank" rel="noreferrer" className="inline-flex items-center gap-2 text-sm md:text-lg group hover:text-drip-black/80" href="https://twitter.com/intent/tweet?url=https://khazifire.com&text=DripUI%20is%20a%20collection%20of%20free%20Tailwind%20CSS%20components%20that%20can%20be%20used%20in%20your%20next%20project.%20%0A-%20By%20@khazifire%0A"><i className="text-lg ri-twitter-line group-hover:-rotate-12 "></i> Share on twitter</a>
                      
                   </div>
               </nav>
