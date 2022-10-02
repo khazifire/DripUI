65a56a410ecbee5f145e4a292311d1ddf05495f6
Udoka <udochukwukaonyela@gmail.com>
2022-10-02 20:46:56 +0100

    created basic nav page

--- components/drip/navbar/basicNavbars.js
+++ components/drip/navbar/basicNavbars.js
@@ -1,5 +1,72 @@
 export const basicNavbars = [
     {
-        type: "Basic Navbar With animated Hamburger menu"
+        type: "Basic Navbar With animated Hamburger menu",
+        category: [
+            {
+                id: "Navbar-1",
+                style: "flex justify-between items-center relative py-3 lg:px-12 px-6 text-white w-10/12 mx-auto",
+                labelStyle: "lg:hidden w-10 h-1 relative rounded-full bg-white after:w-10 after:h-1 after:rounded-full after:bg-white before:w-10 before:h-1 before:rounded-full before:bg-white after:absolute after:top-2 before:-top-2 before:absolute cursor-pointer peer-checked:before:top-0 peer-checked:after:top-0 peer-checked:after:rotate-45 peer-checked:before:-rotate-45  after:transition-transform after:duration-500 before:transition-transform before:duration-500 ease-in peer-checked:bg-transparent",
+                listContainerStyle: "lg:flex lg:translate-x-0 lg:peer-checked:static absolute lg:static -translate-x-[100vh] lg:basis-3/5 justify-evenly font-black peer-checked:translate-x-0 top-[88px] transition-transform duration-500 left-0 right-0  bg-black lg:bg-transparent",
+                children: [
+                    {
+                        id: "About-1",
+                        text: "About",
+                        style: "p-6 cursor-pointer flex lg:p-0 hover:bg-gray-500 transition-colors duration-700 ease-in",
+                        href: "/docs/header"
+                    },
+                    {
+                        id: "Career-1",
+                        text: "Career",
+                        style: "p-6 cursor-pointer flex lg:p-0 hover:bg-gray-500 transition-colors duration-700 ease-in",
+                        href: "/docs/header"
+                    },{
+                        id: "Events-1",
+                        text: "Events",
+                        style: "p-6 cursor-pointer flex lg:p-0 hover:bg-gray-500 transition-colors duration-700 ease-in",
+                        href: "/docs/header"
+                    },{
+                        id: "Product-1",
+                        text: "Product",
+                        style: "p-6 cursor-pointer flex lg:p-0 hover:bg-gray-500 transition-colors duration-700 ease-in",
+                        href: "/docs/header"
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        type: "Basic Navbar With animated Hamburger menu",
+        category: [
+            {
+                id: "Navbar-2",
+                style: "flex justify-between items-center relative bg-drip-black py-3 lg:px-12 px-6 text-white w-10/12 mx-auto",
+                labelStyle: "lg:hidden w-10 h-1 relative rounded-full bg-transparent after:w-8 after:h-1 after:rounded-full after:bg-white before:w-8 before:h-1 before:rounded-full before:bg-white after:absolute after:top-2 before:-top-2 before:absolute cursor-pointer peer-checked:before:top-0 peer-checked:after:top-0 peer-checked:after:rotate-45 peer-checked:before:-rotate-45  after:transition-transform after:duration-500 before:transition-transform before:duration-500 ease-in peer-checked:bg-transparent",
+                listContainerStyle: "lg:flex lg:translate-x-0 lg:peer-checked:static absolute lg:static -translate-x-[100vh] lg:basis-3/5 justify-evenly font-black peer-checked:translate-x-0 top-[88px] transition-transform duration-500 left-0 right-0  bg-black lg:bg-transparent",
+                children: [
+                    {
+                        id: "About-1",
+                        text: "About",
+                        style: "p-6 cursor-pointer flex lg:p-0 hover:bg-gray-500 transition-colors duration-700 ease-in",
+                        href: "/docs/header"
+                    },
+                    {
+                        id: "Career-1",
+                        text: "Career",
+                        style: "p-6 cursor-pointer flex lg:p-0 hover:bg-gray-500 transition-colors duration-700 ease-in",
+                        href: "/docs/header"
+                    },{
+                        id: "Events-1",
+                        text: "Events",
+                        style: "p-6 cursor-pointer flex lg:p-0 hover:bg-gray-500 transition-colors duration-700 ease-in",
+                        href: "/docs/header"
+                    },{
+                        id: "Product-1",
+                        text: "Product",
+                        style: "p-6 cursor-pointer flex lg:p-0 hover:bg-gray-500 transition-colors duration-700 ease-in",
+                        href: "/docs/header"
+                    },
+                ]
+            }
+        ]
     }
 ] 
