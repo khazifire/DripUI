d91f4cfcb4ebb784920dae167c209969ffd4d711
Udoka <udochukwukaonyela@gmail.com>
2022-10-02 21:53:28 +0100

    styling

--- pages/docs/headers.js
+++ pages/docs/headers.js
@@ -4,33 +4,14 @@ import { useRef, useState } from "react";
 import { BasicBtns } from "../../components/drip/buttons/basicBtsns";
 import { ColoredShadowBtns } from "../../components/drip/buttons/coloredShadowBtns";
 import { basicNavbars } from "../../components/drip/navbar/basicNavbars";
+import Navbar from "../../components/drip/navbar/Navbar";
 import Meta from "../../components/layout/meta";
 import PageHeading from "../../components/other/PageHeadings";
 
 export default function Buttons() {
-  // const btnAreaRef = useRef(null);
-  const [CopySuccess, setCopySuccess] = useState(false);
-  const [btnId, setBtnId] = useState();
-
-  const copyToClipboard = async (e) => {
-    setCopySuccess(false);
-    try {
-      await navigator.clipboard.writeText(
-        e.target.outerHTML
-      );
-      setCopySuccess(true);
-      setBtnId(buttonId);
-    } catch (error) {
-      setCopySuccess(false);
-    }
-    setTimeout(() => {
-      setCopySuccess(false);
-      setBtnId();
-    }, 4000);
-  };
 
   return (
-    <>
+    <section className="lg:px-12 px-3 ">
       <Meta
         title={"DripUI - Tailwind CSS Buttons"}
         description={
@@ -46,53 +27,12 @@ export default function Buttons() {
         }
       />
 
-      <section className="flex flex-col">
-          <h2 className="text-2xl font-semibold">Basic Navbar</h2>
-        <div className="flex flex-col">
-          {basicNavbars.map((navbar, index) => (
-            <div key={"Nav" + index}>
-              <p>{navbar.type}</p>
-              <div className="flex flex-col w-10/12 mx-auto mt-8 gap-8 p-4 rounded-lg shadow-lg shadow-slate-500/30 mb-72">
-                {navbar.category.map((nav) => (
-                    <nav
-                        tabIndex={5}
-                        key={nav.id}
-                        onClick={copyToClipboard}
-                    className={nav.style}
-                  >
-                    <input
-                      type={"checkbox"}
-                      id={`${nav.id}-checkbox`}
-                      className="hidden peer"
-                    />
-                        <div className="w-16 h-16 basis-16 rounded-full bg-white text-drip-black font-bold flex items-center justify-center">
-                      Logo
-                    </div>
-                    <ul className={nav.listContainerStyle}>
-                      {nav.children.map((child) => (
-                        <li key={child.id}>
-                          <Link href={child.href}>
-                            <span className={child.style}>{child.text}</span>
-                          </Link>
-                        </li>
-                      ))}
-                        </ul>
-                        <label htmlFor={`${nav.id}-checkbox`} className={nav.labelStyle}></label>
-                  </nav>
-                ))}
-                      <button className="rounded-lg ring-2 w-fit py-3 px-6 mx-auto ring-drip-black hover:bg-drip-black hover:text-white transition-colors duration-500 ease-in">Copy To clipboard</button>
-                {CopySuccess ? (
-                  <span className="absolute inline-flex gap-1 py-2 text-sm -bottom-8">
-                    Copied! <i className="ri-chat-smile-2-line animate-bounce"></i>{" "}
-                  </span>
-                ) : (
-                  ""
-                )}
-              </div>
-            </div>
-          ))}
+      <section className="flex flex-col gap-6">
+          <h2 className="text-3xl font-semibold">Basic Navbar&apos;s</h2>
+        <div className="flex flex-col lg:gap-8">
+          {basicNavbars.map((navbar, index) => (<Navbar key={"Nav" + index} {...{ navbar }} />))}
         </div>
       </section>
-    </>
+    </section>
   );
 }
