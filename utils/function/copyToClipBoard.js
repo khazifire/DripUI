89d8083a2b02730b6c0ad3925bf38ac22b0fceaf
Udoka <udochukwukaonyela@gmail.com>
2022-10-04 10:51:35 +0100

    added copy to clip board logic

--- utils/function/copyToClipBoard.js
+++ utils/function/copyToClipBoard.js
@@ -2,11 +2,11 @@ export default async function copyToClipboard(EL, setCopySuccess) {
     setCopySuccess(false);
     try {
         await navigator.clipboard.writeText(EL.current.outerHTML);
-         setCopySuccess(true);
+        return setCopySuccess(true);
     } catch (error) {
          setCopySuccess(false);
     }
-  return setTimeout(() => {
+  return  setTimeout(() => {
         return setCopySuccess(false);
     }, 4000);
 };
