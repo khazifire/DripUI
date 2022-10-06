f7dda2bb57ca0f86a3d57b507061c99d630680c3
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-10-06 02:31:07 +0100

    Changed 'class' to 'className'

--- components/drip/footer/Footer.js
+++ components/drip/footer/Footer.js
@@ -1,26 +1,26 @@
 
-<footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
-    <div class="sm:flex sm:items-center sm:justify-between">
-        <a href="https://dripui.vercel.app/" class="flex items-center mb-4 sm:mb-0">
+<footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+    <div className="sm:flex sm:items-center sm:justify-between">
+        <a href="https://dripui.vercel.app/" className="flex items-center mb-4 sm:mb-0">
             
-            <span class="self-center text-2xl font-semibold whitespace-nowrap font-Cursive">DripUI</span>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap font-Cursive">DripUI</span>
         </a>
-        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
             </li>
             <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
             </li>
             <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+                <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
             </li>
             <li>
-                <a href="#" class="hover:underline">Contact</a>
+                <a href="#" className="hover:underline">Contact</a>
             </li>
         </ul>
     </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
-    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://dripui.vercel.app/" class="hover:underline">DripUI™</a>. All Rights Reserved.
+    <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://dripui.vercel.app/" className="hover:underline">DripUI™</a>. All Rights Reserved.
     </span>
 </footer>
