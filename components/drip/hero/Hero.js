72168a25a738dede4396e3595e5a8fc21ab55c93
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-10-06 03:03:36 +0100

    Removed ' on line 23

--- components/drip/hero/Hero.js
+++ components/drip/hero/Hero.js
@@ -20,7 +20,7 @@ function Hero() {
           <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 bg-gradient-to-b from-blue-400 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">The World's<br/> Fastest Growing</span>{' '}
+                <span className="block xl:inline">The Worlds<br/> Fastest Growing</span>
                 <span className="block text-blue-600 xl:inline">Crypto Investment</span>
               </h1>
               <p className="mt-3 text-base text-white-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0">
