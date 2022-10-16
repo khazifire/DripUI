a874b8d7531acffeff83f924802da5b6f5b3aff7
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-10-16 08:18:10 +0100

    Created StickyHeader

--- components/drip/footer/Footer.js
+++ components/drip/footer/Footer.js
@@ -1,3 +1,5 @@
+function Footer() {
+  return (
 
 <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
     <div className="sm:flex sm:items-center sm:justify-between">
@@ -24,3 +26,7 @@
     <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://dripui.vercel.app/" className="hover:underline">DripUI™</a>. All Rights Reserved.
     </span>
 </footer>
+);
+}
+
+export default Footer
