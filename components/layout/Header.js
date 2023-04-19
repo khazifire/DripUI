d5a4477d9045989dbffc864c825e846eee79d5c2
davidhanenko <d.hanenko24@gmail.com>
2023-04-18 20:30:08 -0400

    Inconsistent Responsive MENU / Navbar on different mobiles

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,28 +1,26 @@
-import Link from "next/link";
-import Navigation from "./Navigation";
+import Link from 'next/link';
+import Navigation from './Navigation';
 
 const Header = () => {
-
   return (
-    <header className="sticky top-0 w-full border-b-2 bg-drip-white border-drip-gray-light z-50 relative">
-      <nav className="flex flex-row items-center justify-between py-6 custom-container">
-        <div className="flex flex-row items-center gap-4">
-          <Link href="/">
-            <a className="text-2xl font-bold md:text-3xl font-Cursive">
+    <header className='sticky top-0 w-full border-b-2 bg-drip-white border-drip-gray-light z-50 relative'>
+      <nav className='flex flex-row items-center justify-between py-6 px-4'>
+        <div className='flex flex-row items-center gap-4'>
+          <Link href='/'>
+            <a className='text-2xl font-bold md:text-3xl font-Cursive'>
               DripUI
             </a>
           </Link>
 
-
-          <Link href="/docs">
-            <a className="pl-3 text-sm border-l-2 md:text-lg border-drip-black/20 hover:text-drip-black/80">
+          <Link href='/docs'>
+            <a className='pl-3 text-sm border-l-2 md:text-lg border-drip-black/20 hover:text-drip-black/80'>
               Components
             </a>
           </Link>
 
           <a
             disabled
-            className="pl-2 text-sm pointer-events-none md:text-lg text-drip-black/20"
+            className='pl-2 text-sm pointer-events-none md:text-lg text-drip-black/20'
           >
             Web Designs
           </a>
