3711c250ee2a02d8b5a9aa837be35b3812dee48b
David Hanenko <d.hanenko24@gmail.com>
2023-04-21 04:01:32 -0400

    Inconsistent Responsive MENU / Navbar on different mobiles (#44)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend:{
+    extend: {
       colors: {
-        'drip':{
-          'black':'#141b1f',
-          'gray': {
-            'light':'#EEEEEE',
-            'dark':'#393E46'
+        drip: {
+          black: '#141b1f',
+          gray: {
+            light: '#EEEEEE',
+            dark: '#393E46',
           },
-          'yellow': '#f8c238',
-          'white': '#ffffff',
+          yellow: '#f8c238',
+          white: '#ffffff',
         },
         transparent: 'transparent',
         current: 'currentColor',
       },
     },
     fontFamily: {
-      Raleway :['Raleway', 'sans-serif'],
+      Raleway: ['Raleway', 'sans-serif'],
       Cursive: ['Caveat', 'cursive'],
       CursiveBrush: ['Caveat Brush', 'cursive'],
     },
   },
   plugins: [],
-}
+};
