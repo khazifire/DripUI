6c04f4e17779f5f65fbea877674bc3d23c0af8d4
keerthi538 <keerthikeerthik275@gmail.com>
2023-12-26 17:32:17 +0530

    fix: Resolve the error coming up during local project setup  refer https://nextjs.org/docs/messages/react-hydration-error  relates to issue #53 https://github.com/khazifire/DripUI/issues/53

--- hooks/useWindowDimensions.js
+++ hooks/useWindowDimensions.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 
+const initDimensions = {
+  height: 0,
+  width: 0,
+};
+
 export default function useWindowDimensions() {
-
-  const hasWindow = typeof window !== 'undefined';
-
-  function getWindowDimensions() {
+  function getWindowDimensions(hasWindow) {
     const width = hasWindow ? window.innerWidth : null;
     const height = hasWindow ? window.innerHeight : null;
     return {
@@ -13,18 +15,21 @@ export default function useWindowDimensions() {
     };
   }
 
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = useState(initDimensions);
 
   useEffect(() => {
+    const hasWindow = typeof window !== 'undefined';
     if (hasWindow) {
       function handleResize() {
-        setWindowDimensions(getWindowDimensions());
+        setWindowDimensions(getWindowDimensions(hasWindow));
       }
 
+      // Gets window size for the first time
+      handleResize();
       window.addEventListener('resize', handleResize);
       return () => window.removeEventListener('resize', handleResize);
     }
-  }, [hasWindow]);
+  }, []);
 
   return windowDimensions;
 }
