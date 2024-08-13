42de7198ac11c6e9abd5b147e9ed35cda03878c1
itsnooshin <nooshindev@gmail.com>
2024-08-13 16:18:37 +0330

    fix(nav-mobile):refactor the navigation mobile and create a animation when the menu open 38(#45)

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -1,45 +1,45 @@
-import Link from 'next/link';
-import { useRef, useState } from 'react';
-
-import useWindowDimensions from '../../hooks/useWindowDimensions';
-import useOutsideClick from '../../hooks/useClickOutside';
-import { HiOutlineMenu } from 'react-icons/hi';
+import Link from "next/link";
+import { useRef, useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
 
 const TWITTER_LINK =
-  'https://twitter.com/intent/tweet?url=https://khazifire.com&text=DripUI%20is%20a%20collection%20of%20free%20Tailwind%20CSS%20components%20that%20can%20be%20used%20in%20your%20next%20project.%20%0A-%20By%20@khazifire%0A';
-const GITHUB_LINK = 'https://github.com/khazifire/DripUI';
+  "https://twitter.com/intent/tweet?url=https://khazifire.com&text=DripUI%20is%20a%20collection%20of%20free%20Tailwind%20CSS%20components%20that%20can%20be%20used%20in%20your%20next%20project.%20%0A-%20By%20@khazifire%0A";
+const GITHUB_LINK = "https://github.com/khazifire/DripUI";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const { height, width } = useWindowDimensions();
-  const ref = useRef(null);
-  useOutsideClick(ref, setDefault);
-
-  const styleMobile = `${
-    isOpen
-      ? 'flex absolute flex-col justify-evenly items-end mt-2 right-0 bg-black text-white p-3 w-64 min-w-fit'
-      : 'hidden'
-  }`;
   function handleOnClick() {
     setIsOpen(!isOpen);
   }
 
-  function setDefault() {
-    setIsOpen(false);
-  }
+  return (
+    <>
+      <div className="md:hidden block">
+        {isOpen ? (
+          <button onClick={handleOnClick}>
+            <IoClose size={"20px"} />
+          </button>
+        ) : (
+          <button onClick={handleOnClick}>
+            {" "}
+            <FaBars />
+          </button>
+        )}
+      </div>
 
-  if (width < 700) {
-    return (
-      <div ref={ref}>
-        <HiOutlineMenu
-          onClick={handleOnClick}
-          className='hover:cursor-pointer'
-        />
-        <div className={styleMobile}>
-          <Link href='/docs/how-to-use'>
+      <div
+        className={`md:hidden fixed top-20 right-0 h-[100vh] w-full bg-white text-black overflow-hidden transition-all duration-700 ease-in-out ${
+          isOpen
+            ? "transform translate-x-0 scale-100 opacity-100"
+            : "transform translate-x-full scale-95 opacity-0"
+        }`}
+      >
+        <div className=" absolute top-4 right-3"></div>
+        <div className="flex flex-col items-center justify-center  h-[50vh] gap-9">
+          <Link href="/docs/how-to-use">
             <a
-              className='pr-3 text-sm border-b-2 border-drip-black/20 hover:text-drip-white/80'
+              className="pr-3 text-sm border-b-2 border-drip-black/20 hover:text-drip-white/80"
               onClick={handleOnClick}
             >
               How to use
@@ -47,60 +47,57 @@ const Navigation = () => {
           </Link>
 
           <a
-            target='_blank'
-            rel='noreferrer'
-            className='inline-flex items-center gap-2 pr-3 text-sm group hover:text-drip-white/80 border-b-2 border-drip-black/20'
+            target="_blank"
+            rel="noreferrer"
+            className="inline-flex items-center gap-2 pr-3 text-sm group hover:text-drip-white/80 border-b-2 border-drip-black/20"
             onClick={handleOnClick}
             href={TWITTER_LINK}
           >
-            <i className='text-lg ri-twitter-line group-hover:-rotate-12'></i>{' '}
+            <i className="text-lg ri-twitter-line group-hover:-rotate-12"></i>{" "}
             Share on twitter
           </a>
 
           <a
-            target='_blank'
-            rel='noreferrer'
+            target="_blank"
+            rel="noreferrer"
             href={GITHUB_LINK}
-            className='inline-flex items-center gap-2 pr-3 text-sm group hover:text-drip-white/80 '
+            className="inline-flex items-center gap-2 pr-3 text-sm group hover:text-drip-white/80 "
             onClick={handleOnClick}
           >
-            <i className='text-lg ri-github-line group-hover:-rotate-12'></i>
+            <i className="text-lg ri-github-line group-hover:-rotate-12"></i>
             Visit on Github
           </a>
         </div>
       </div>
-    );
-  }
 
-  return (
-    <div className='flex flex-row gap-4 items-center'>
-      <Link href='/docs/how-to-use'>
-        <a className='pr-3 text-sm border-r-2 md:text-lg border-drip-black/20 hover:text-drip-black/80'>
-          How to use
+      <div className="hidden  md:flex flex-row gap-4 items-center">
+        <Link href="/docs/how-to-use">
+          <a className="pr-3 text-sm border-r-2 md:text-lg border-drip-black/20 hover:text-drip-black/80">
+            How to use
+          </a>
+        </Link>
+
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="inline-flex items-center  gap-2 pr-3 text-sm md:text-lg group hover:text-drip-black/80 border-r-2 border-drip-black/20"
+          href={TWITTER_LINK}
+        >
+          <i className="text-lg ri-twitter-line group-hover:-rotate-12 "></i>{" "}
+          Share on twitter
         </a>
-      </Link>
 
-      <a
-        target='_blank'
-        rel='noreferrer'
-        className='inline-flex items-center gap-2 pr-3 text-sm md:text-lg group hover:text-drip-black/80 border-r-2 border-drip-black/20'
-        href={TWITTER_LINK}
-      >
-        <i className='text-lg ri-twitter-line group-hover:-rotate-12 '></i>{' '}
-        Share on twitter
-      </a>
-
-      <a
-        target='_blank'
-        rel='noreferrer'
-        href={GITHUB_LINK}
-        className=' hover:text-drip-black/80 hover:-rotate-12'
-      >
-        <i className='text-lg ri-github-line'></i>
-      </a>
-    </div>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={GITHUB_LINK}
+          className=" hover:text-drip-black/80 hover:-rotate-12"
+        >
+          <i className="text-lg ri-github-line"></i>
+        </a>
+      </div>
+    </>
   );
 };
 
 export default Navigation;
-
