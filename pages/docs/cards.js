f06cebe881997f6e032e68c588101a5801afed38
Sameer-Saleem1 <sameersaleemq75@gmail.com>
2025-09-25 16:30:05 +0500

    added a new card for userprofile that displays the image of the user, name, designation and two button inside the card of 'Add friend' and 'Message'. Also added the copyToClipboard functionality so that when user clicks the button it will copy the code of this card to their clipboard. Also fixed the grid system of the card into a more representable way

--- pages/docs/cards.js
+++ pages/docs/cards.js
@@ -8,6 +8,7 @@ export default function Cards() {
   const cardAreaRef2 = useRef(null);
   const cardAreaRef3 = useRef(null);
   const cardAreaRef4 = useRef(null);
+  const cardAreaRef5 = useRef(null);
   const [CopySuccess, setCopySuccess] = useState(false);
   const [cardId, setCardId] = useState();
 
@@ -42,7 +43,7 @@ export default function Cards() {
           "The Card component is used to show data and various information to users; such as blog post, user profiles, image galleries and many more."
         }
       />
-      <section className="grid grid-cols-1 gap-16 md:grid-cols-4">
+      <section className="grid grid-cols-1 gap-15 md:grid-cols-3 gap-5">
         {/* <DefaultCard /> */}
         <div
           ref={cardAreaRef1}
@@ -160,6 +161,101 @@ export default function Cards() {
             </div>
           </div>
           {CopySuccess && cardId == "card4" ? (
+            <span className="absolute inline-flex gap-1 py-2 text-sm -top-7">
+              Copied! <i className="ri-chat-smile-2-line animate-bounce"></i>{" "}
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
+
+        {/* UserProfile card */}
+
+        <div
+          ref={cardAreaRef5}
+          onClick={() => copyToClipboard(cardAreaRef5, "card5")}
+          className="relative flex flex-col gap-2 "
+        >
+          <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow shadow-drip-black/30 cursor-pointer  dark:bg-gray-800 dark:border-gray-700 hover:shadow-lg">
+            <div class="flex justify-end px-4 pt-4">
+              <button
+                id="dropdownButton"
+                data-dropdown-toggle="dropdown"
+                class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+                type="button"
+              >
+                <span class="sr-only">Open dropdown</span>
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 16 3"
+                >
+                  <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                </svg>
+              </button>
+              <div
+                id="dropdown"
+                class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700"
+              >
+                <ul class="py-2" aria-labelledby="dropdownButton">
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Edit
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Export Data
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                    >
+                      Delete
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="flex flex-col items-center pb-10">
+              <img
+                class="w-24 h-24 mb-3 rounded-full shadow-lg"
+                src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
+                alt="Bonnie image"
+              />
+              <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                Bonnie Green
+              </h5>
+              <span class="text-sm text-gray-500 dark:text-gray-400">
+                Visual Designer
+              </span>
+              <div class="flex mt-4 md:mt-6 gap-4">
+                <a
+                  href="#"
+                  class="inline-flex  items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Add friend
+                </a>
+                <a
+                  href="#"
+                  class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                >
+                  Message
+                </a>
+              </div>
+            </div>
+          </div>
+          {CopySuccess && cardId == "card5" ? (
             <span className="absolute inline-flex gap-1 py-2 text-sm -top-7">
               Copied! <i className="ri-chat-smile-2-line animate-bounce"></i>{" "}
             </span>
